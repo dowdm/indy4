ab77c900c1f7df4eb324e03aab3bb41ccaafa510
Matt Dowdney <theofficialmatt@gmail.com>
2018-04-06 10:28:08 -0700

    add price prototype for summing cost of pie

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,32 @@
-var pieCost = 0;
-function Pizza (size, toppingCost){
-  this.size = size;
+function Pizza (sizeCost, toppingCost){
+  this.sizeCost = sizeCost;
   this.toppingCost = toppingCost;
 }
 
 Pizza.prototype.price = function(array){
-  var toppingsTota
-  pieCost = parseInt(this.size) +
-}
+  var arrayTotal = 0;
+  for (var i = 0; i < array.length; i++) {
+    arrayTotal += array[i];
+    console.log(arrayTotal);
+  }
+  return this.sizeCost + arrayTotal;
+  // console.log(arrayTotal);
+  // console.log(this.sizeCost);
+  }
+
+$(document).ready(function(){
+  $("form#pie-order").submit(function(event){
+    event.preventDefault();
+    var pieSizeCost = parseInt($("#size").val());
+    var toppingArray = [];
+    $("input:checkbox[name=toppings]:checked").each(function(){
+      var oneCost = parseInt($(this).val());
+      toppingArray.push(oneCost);
+    });
+
+    var pie = new Pizza(pieSizeCost, toppingArray);
+    var piePrice = pie.price(pie.toppingCost);
+    console.log(pie);
+    console.log(piePrice);
+  });
+});
